adacd8af73eabdde798952bc39c6cf8398edb475
Abhishek Rai <raigon48@gmail.com>
2025-09-24 14:47:38 +0530

    add myuser route

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import express from "express";
-import { type Request, type Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import myUserRoute from "./routes/myUserRoutes";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
   console.log("Connected to database");
@@ -13,9 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
-  return res.json({ message: "Hello" });
-});
+app.use("/api/my/user", myUserRoute);
 
 app.listen(3001, () => {
   console.log("Server is listening on localhost:3001");
